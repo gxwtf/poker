3fce1c3cecd1f17b8547afdab9ce80cf398346d3
Wangzehao2009 <wangzehaosimba@gmail.com>
2026-02-27 06:47:19 +0000

    fix: redirect

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 const configureRoutes = (app) => {
   app.use('/api/auth', require('./api/auth'));
   app.use('/api/users', require('./api/users'));
   app.use('/api/mails', require('./api/mails'));
   app.use('/api/chips', require('./api/chips'));
   app.use('/', (req, res) => {
-    res.status(200).send('Welcome to Vintage Poker!');
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
   });
 };
 
